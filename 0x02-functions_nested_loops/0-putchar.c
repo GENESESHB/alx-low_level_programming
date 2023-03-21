6bd491a9eaf68573daa6d7bd0cf7530d0edc5e7c
GENESESHB <hassan.hbmama@gmail.com>
2023-03-21 07:26:39 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - Print "_putchar" folloed by a new line.
+*
+* Description: You are not allowed to include standard libraries.
+* Return: 0
+*/
+int main(void)
+{
+	char putchar[] = "_putchar";
+	int i = 0;
+
+	while (putchar[i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
+	_putchar ('\n');
+
+	return (0);
+}
