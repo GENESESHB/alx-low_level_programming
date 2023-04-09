80cea5161a6d3d559a1b190ad1d9344fb592c1ed
GENESESHB <hassan.hbmama@gmail.com>
2023-04-08 19:10:54 -0700

    my arry aff number of arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main- prints programme name
- *@argc: numbers of the argumants into the progrmme
- *@argv:the arry of the programme
- *return: 0
- */
-
-int main(int _mynameis_((_mynewnameis_)) argc, char *argv[])
+* main - prints program name
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
