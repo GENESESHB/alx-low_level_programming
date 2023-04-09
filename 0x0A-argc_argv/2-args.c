10850275e7777b516ae1e886df11b3572297ec1c
GENESESHB <hassan.hbmama@gmail.com>
2023-04-09 11:47:07 -0700

    Write a program that prints all arguments it receives.
    All arguments should be printed, including the first one
    Only print one argument per line, ending with a new line

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,12 +7,11 @@
 *Return: returns 0
 */
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-                 int i;
-		 
-		 for (i = 0; i < argc; i++)
-			 printf("%s\n", argv[i]);
-				 
-	         return (0);
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
 }
