81fc98546d573ebf6be774dbf00187ad0e4d039f
GENESESHB <hassan.hbmama@gmail.com>
2023-04-14 08:06:23 -0700

    Write a function that reallocates a memory block using malloc and free
    Prototype: void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,7 +12,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 int len(char *str);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void *_realloc(void *ptr, signed int old_size, signed int new_size);
+void *realloc(void *ptr, signed int old_size, signed int new_size);
 int find_len(char *str);
 char *create_xarray(int size);
 char *iterate_zeroes(int size);
