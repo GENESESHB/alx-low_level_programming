721e84a9f0408cf9f738387f4e4ad397f34bf22d
HASSANBOUDRRA <hassan.hbmama@gmail.com>
2023-04-27 18:27:49 -0700

    tasks4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: Pointer to the head of the list.
+ */
+void free_list(list_t *head)
+{
+list_t *current;
+
+while (head)
+{
+current = head;
+head = head->next;
+free(current->str);
+free(current);
+}
+}
