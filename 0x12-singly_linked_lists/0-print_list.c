e0109c615e396e7388e408acb9dfdb4be69c2e1a
HASSANBOUDRRA <hassan.hbmama@gmail.com>
2023-04-27 18:06:23 -0700

    tasks0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,25 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h; singly linked list.
- * Return: number of elements in the list.
+ * print_list - Prints all the elements of a list_t list
+ * @h: Pointer to the head of the list
+ *
+ * Return: The number of nodes
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+		if (!h->str)
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		i++;
+		count++;
 	}
-	return (i);
+
+	return (count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,11 @@
-#ifndef _LIST_
-#define _LIST_
+#ifndef LISTs_H_
+#define LISTs_H_
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -7,20 +13,21 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
+
+/* name of prototype */
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-int _strlen(const char *s);
-void before_main(void)__attribute__((constructor));
+
 #endif
