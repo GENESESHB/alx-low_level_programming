c3743115f508dd849feb0174e5242bd500e1a541
HASSANBOUDRRA <hassan.hbmama@gmail.com>
2023-05-04 11:50:23 -0700

    TASKS2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,7 +12,7 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (n == 0 && index < 64)
 		return (0);
-	for (v = 0; v <= 63; n >>= 1,v++)
+	for (v = 0; v <= 63; n >>= 1, v++)
 	{
 		if (index == v)
 			return (n & 1);
