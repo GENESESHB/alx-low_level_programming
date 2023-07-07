f77affadca6f2b30e4efcca5994dce825a4baf66
HASSANBOUDRRA <hassan.hbmama@gmail.com>
2023-07-07 14:19:34 -0700

    hh

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -43,4 +43,6 @@ hash_node_t *set_pair(const char *key, const char *value);
 int set_pair_only(hash_table_t *ht, const char *key, const char *value, unsigned long int index);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+
 #endif
