ef9e219d7aadb3724213cfb047a456267b2bc5c7
HASSANBOUDRRA <hassan.hbmama@gmail.com>
2023-07-07 14:17:05 -0700

    the functioncan retreives the value of a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /*
- * hash_table_get - the function can retrievis the value of a key
+ *hash_table_get - the function can retrievis the value of a key
  * @ht: is the hash table you want to look into
  * @key: is the key you are looking for
  *
- * Returns: the value associated with the element, or NULL if key couldn’t be found
+ * Returns: the value associated with the element, or NULL if key can't found
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
