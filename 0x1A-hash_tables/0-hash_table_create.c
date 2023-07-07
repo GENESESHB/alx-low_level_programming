6f090daa573d3506d4f1099f0a8bdd822a226383
HASSANBOUDRRA <hassan.hbmama@gmail.com>
2023-07-07 07:55:28 -0700

    creat a hash tables associed with array and a size with malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,33 +1,27 @@
 #include "hash_tables.h"
-
-/*
- * hash_tables_create - create a hash tables .
- * @size: the size of array
- *
- * Return: A pointer the nEw hash tables created or | NULL for faild progress
+/**
+ *hash_table_create -  a function that creates a hash table.
+ *@size: the size of array of pointers.
+ *Return: a pointer to the newly created hash table
+ *otherwise NULL.
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
+	hash_table_t *ht;
 	unsigned long int i;
 
-	hash_table = malloc(sizeof(hash_table_t));
+	hash_table = malloc(sizeof(hash_table_t) * 1);
 	if (hash_table == NULL)
 		return (NULL);
 
-	hash_table->array = malloc(sizeof(hash_table_t *) * size);
-	if (hash_table->array == NULL)
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 	{
 		free(hash_table);
 		return (NULL);
 	}
-
-	hash_table->size = size;
-	
-	for (i = 0; i < size; i++);
-
-	hash_table->array[i] = NULL;
-
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
 	return (hash_table);
 }
