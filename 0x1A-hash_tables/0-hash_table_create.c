6a07dd4df3d93d3b5ce09f209b55dacf2b7564e6
HASSANBOUDRRA <hassan.hbmama@gmail.com>
2023-07-07 08:00:52 -0700

    creat a hash tables associed with array and a size with malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *hash_table;
 	unsigned long int i;
 
 	hash_table = malloc(sizeof(hash_table_t) * 1);
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	if (hash_table->array == NULL)
 	{
 		free(hash_table);
 		return (NULL);
