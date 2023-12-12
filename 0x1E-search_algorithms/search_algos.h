110549a717c6294b05b075171e5aec932037a029
root <root@DESKTOP-MOC38TV>
2023-12-13 00:39:35 +0100

    1 tasks with lmove

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGO_H
+#define SEARCH_ALGO_H
+
+
+#include <stdio.h>
+#include <stddef.h>
+
+
+
+
+int linear_search(int *array, size_t size, int value);
+
+#endif  /* search_algos */
